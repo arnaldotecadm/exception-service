8aec4dd6f2a6b83c32f06fe20ea8d1f75e335a64
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2020-09-19 23:26:05 -0300

    alterado o schema default

--- src/main/java/br/com/emerion/model/ExceptionModel.java
+++ src/main/java/br/com/emerion/model/ExceptionModel.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -27,6 +28,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @Entity(name = "exception_management")
+@Table(schema = "asoft")
 public class ExceptionModel implements Serializable, Comparable<ExceptionModel> {
 
 	/**
